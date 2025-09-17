575b7c5fe9327d7ba6e2af338712a0f668972924
Purnendu Tiwari <purnendutiwari2004@gmail.com>
2025-09-18 01:05:09 +0530

    add css styles

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <ScrollRestoration />
+        <ScrollRestoration /> 
         <Scripts />
       </body>
     </html>
